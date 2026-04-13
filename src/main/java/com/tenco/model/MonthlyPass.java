1b98ccb6d90f463bd69d07a95238b3819185d495
tyegwan <tye4703@naver.com>
2026-04-13 15:25:57 +0900

    feat: model구현

--- src/main/java/com/tenco/model/MonthlyPass.java
+++ src/main/java/com/tenco/model/MonthlyPass.java
@@ -1,4 +1,22 @@
 package com.tenco.model;
 
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+
 public class MonthlyPass {
+    private int passId;
+    private String carNumber;
+    private String ownerName;
+    private Date startDate;
+    private Date endDate;
+    private BigDecimal fee;
 }
