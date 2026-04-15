1f6fb180b64894ffcf55aa5e580bb914665751a8
mskimdev <aksen1754@naver.com>
2026-04-15 16:26:08 +0900

    feat: admin DTO 구현

---
+++ src/main/java/com/tenco/model/Admin.java
@@ -0,0 +1,19 @@
+package com.tenco.model;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class Admin {
+    private int id;
+    private String userId;
+    private String password;
+    private String name;
+    private LocalDateTime createdAt;
+    private boolean isAvailable;
+}
